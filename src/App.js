1622adc328d1f12685c858232b0293e0c8998e51
Dominick <d3lee@ucsd.edu>
2020-11-06 12:40:30 -0800

    ☕️ Login elements are all there

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import LoginPage from './login';
+
+function App() {
+  const login = true;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginPage></LoginPage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/base.js
@@ -0,0 +1,17 @@
+import firebase from "firebase"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDw_KpzmTnlhuekTqy-uimV9YRO1_ZlIb8",
+    authDomain: "philanthropy-connect.firebaseapp.com",
+    databaseURL: "https://philanthropy-connect.firebaseio.com",
+    projectId: "philanthropy-connect",
+    storageBucket: "philanthropy-connect.appspot.com",
+    messagingSenderId: "468368677937",
+    appId: "1:468368677937:web:37a6ff04b377434d008910",
+    measurementId: "G-JQKPX8C949"
+};
+
+// Initialize Firebase
+const db = firebase.initializeApp(firebaseConfig);
+
+export default db;
