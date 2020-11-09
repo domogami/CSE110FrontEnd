e7a0fb9df720b0442a110fb754accfd003d7d4bf
Dominick <d3lee@ucsd.edu>
2020-11-09 13:31:39 -0800

    ➕ Login is starting to look pretty now

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -2,8 +2,9 @@ import React, {useContext} from "react";
 import {AuthContext} from "../../auth/Auth";
 import { Redirect } from "react-router-dom";
 import db from "../common/base";
-import logo from '../common/images/PhilConLogo1.png';
+import logo from '../common/images/logo.svg';
 import "./login.css"
+import Handshake from './handshake'
 
 const Login = ({history}) => {
 
@@ -29,26 +30,34 @@ const Login = ({history}) => {
         return <Redirect to="/" />;
       }
     return (
-        <div>
+        <div className="parent">
+            <div className="title">
             <img src={logo} className="logo" />
             <h1>
             Philanthropy Connect
             </h1>
-           
-            <h1>Log In</h1>
-            <form onSubmit={handleLogin} className="EmailAndPass">
-                <label className="fieldLabel">
-                    Email
-                    <input className="entryField" name="email" type="email" placeholder="ggillespie@eng.ucsd.edu" />
-                </label>
-                <label className="fieldLabel">
-                    Password
-                    <input className="entryField" name="password" type="password" placeholder="··········" />
-                </label>
-                <button className="loginButton" type="submit">Log In</button>
-            </form>
-            <button className="signinButton" onClick={redirectSignUp}>Sign Up</button>
-            
+            </div>
+
+            <div className="loginContainer">
+                <div className="loginInfo">
+                    <h1>Log In</h1>
+                    <form onSubmit={handleLogin} className="EmailAndPass">
+                        <label className="fieldLabel">
+                            Email
+                            <input className="entryField" name="email" type="email" placeholder="ggillespie@eng.ucsd.edu" />
+                        </label>
+                        <label className="fieldLabel">
+                            Password
+                            <input className="entryField" name="password" type="password" placeholder="··········" />
+                        </label>
+                        <button className="loginButton" type="submit">Log In</button>
+                    </form>
+                    <button className="signinButton" onClick={redirectSignUp}>Sign Up</button>
+                </div>
+                <div className="handshake">
+                <Handshake />
+                </div>
+            </div>
         </div>
     );
 };
