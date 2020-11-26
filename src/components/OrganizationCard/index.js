3dd23c2c32e4ff0d70588879d35871da7447241a
Yuu <okumura6883@gmail.com>
2020-11-25 22:25:43 -0800

    Updated css

--- src/components/OrganizationCard/index.js
+++ src/components/OrganizationCard/index.js
@@ -1,49 +1,33 @@
 // TODO 
 import React, {Component} from 'react';
-import { ReactComponent as Logo } from './background.svg'
 import "./orgCards.css"
 
+/**
+ * @extends {Component<{ doc: OrganizationDocument }>}
+ */
 export class OrganizationCard extends Component {
-    // Prop will contain:
-    // 0. Picture source
-    // 1. Name of org
-    // 2. Description
+
     constructor(props) {
         super(props);
         this.state = {
             showDesc: false,
             orgName: "Test",
-            orgDescription: "Description",
-            color: "#555555"
+            orgDescription: "Description"
         };
     }
 
-    hovering(event) {
-        if ( event.type === "mouseover" ){
-            this.setState({
-                showDesc: true,
-                orgDescription: "Over",
-            });
-        }
-        // Event "mouseout"
-        else {
-            this.setState({
-                showDesc: false,
-                orgDescription: "Off",
-            });
-        }
-    }
-
     render() {
-        return <Logo 
-                    className="orgCards"
-                    fill={this.state.color}
-                    onMouseOver={e => this.hovering(e)} 
-                    onMouseOut={e => this.hovering(e)}>
-                    <h2 className="orgText" style={{opacity: (this.state.showDesc ? 255 : 0)}}>
-                        {this.state.orgDescription}
-                    </h2>
-                </Logo>;
+        return (
+            <div className="orgCards"
+                style={this.state.color ? {backgroundColor: this.state.color} : {}}>
+                <h5>{this.props.doc.title}</h5>
+                <div>
+                    <h5 className="orgText">
+                        {this.props.doc.mission}
+                    </h5>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,6 +7,16 @@ import "./style.css"
 class Home extends Component {
 
     render(){
+        const orgDoc = {
+            title: "Habitat For Humanity",
+            mission: "Fixing homelessness one family at a time",
+            cause: ["Medical"],
+            zip: "92122",
+            contact: "info@sandiegohabitat.org",
+            url: "www.sandiegohabitat.org",
+            events: ["new event"]
+        };
+
         return (
             <div>
                 <h1>Home</h1>
@@ -14,12 +24,12 @@ class Home extends Component {
                     {/* TODO */}
                 </div>
                 <div className="orgGrid">
+                    <OrganizationCard doc={orgDoc}/>
+                    {/* <OrganizationCard />
                     <OrganizationCard />
                     <OrganizationCard />
                     <OrganizationCard />
-                    <OrganizationCard />
-                    <OrganizationCard />
-                    <OrganizationCard />
+                    <OrganizationCard /> */}
                 </div>
                 <button onClick={() => db.auth().signOut()}>Sign Out</button>
             </div>
