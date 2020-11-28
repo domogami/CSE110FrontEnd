bb64fee6571d46a028cbdb1ff33b90f609bb95f9
unduwapk <ukandage@ucsd.edu>
2020-11-27 16:34:02 -0800

    changes made to header; not yet complete

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import "./header.css";
 import Logo from "../../routes/common/images/PhilConLogo.png";
 import profilePic from "../../routes/common/images/PhilConLogo.png"
+import { NavLink } from 'react-router-dom';
+import SearchBar from '../searchBar/index';
 
 //logo + title + profilepic?
 //filters/ search bar part 
 
 //activeClassName... 
 
+
 export class Header extends Component {
     render() {
+        var isIndiviual = true;
         return (
             <div>
                 <div className="titleProfile">
@@ -24,11 +28,23 @@ export class Header extends Component {
                 <img className="profilePic" src={profilePic} alt="profilePic"/>
                 </div>
 
+                {/*pageName*/}
+                <div buttonSearch>
+                { isIndiviual ? 
+                <div className="headerButton">
+                    <NavLink activeClassName="active" className="menuButton" to="/"><p>Home</p></NavLink>
+                    <NavLink activeClassName="active" className="menuButton" to="/findOrg"><p>Find Org</p></NavLink>
+                    <NavLink activeClassName="active" className="menuButton" to="/profile"><p>Profile</p></NavLink>
+                </div> : 
+                    <div className="headerButtonOrg">
+                    <NavLink activeClassName="active" className="menuButton" to="/"><p>Home</p></NavLink>
+                    <NavLink activeClassName="active" className="menuButton" to="/findOrg"><p>Post</p></NavLink>
+                    <NavLink activeClassName="active" className="menuButton" to="/findOrg"><p>Stats</p></NavLink>
+                    <NavLink activeClassName="active" className="menuButton" to="/profile"><p>Profile</p></NavLink>
+                </div>
+                }
 
-
-                //pageName
-                <div className="pageName">
-                
+                <SearchBar></SearchBar>
                 </div>
 
             </div>
---
+++ src/components/searchBar/index.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import "./searchBar.css";
+
+export class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+            
+            {/*bar*/}
+            <div>
+                
+            </div>
+
+            {/*magnify*/}
+            <div>
+
+            </div>
+
+            </div>          
+        );
+    }
+}
+
+export default SearchBar;
