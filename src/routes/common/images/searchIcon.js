2847499eff427a8557df072f47e2f49ce3c05983
unduwapk <ukandage@ucsd.edu>
2020-12-01 21:03:44 -0800

    search bar comeplete, i think -- header positioned to the top on all pgs except profile - profile being annoying with positions

--- src/routes/common/images/searchIcon.js
+++ src/routes/common/images/searchIcon.js
@@ -3,8 +3,8 @@ export default function Search() {
 return (
     <div>
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" dataReactroot="">
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="#ffffff" fill="none" d="M9.5 17C13.6421 17 17 13.6421 17 9.5C17 5.35786 13.6421 2 9.5 2C5.35786 2 2 5.35786 2 9.5C2 13.6421 5.35786 17 9.5 17Z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="#ffffff" d="M22 22L14.8 14.8"></path>
+        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="#b153fd" fill="none" d="M9.5 17C13.6421 17 17 13.6421 17 9.5C17 5.35786 13.6421 2 9.5 2C5.35786 2 2 5.35786 2 9.5C2 13.6421 5.35786 17 9.5 17Z"></path>
+        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="#b153fd" d="M22 22L14.8 14.8"></path>
         </svg>
     </div>
 )}
