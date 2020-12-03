58be994242bd44aa2e8abed3d18db16249e58aa2
Dominick <d3lee@ucsd.edu>
2020-12-03 15:19:57 -0800

    😅 Tried to improve the header

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -11,7 +11,7 @@ class Home extends Component {
 
     render(){
         return (
-            <div>
+            <div className="HomePage">
                 <div className="header">
                     <Header />
                 </div>
