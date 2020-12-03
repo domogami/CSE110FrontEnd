212393ff4c23afa19f73c12f03ea08df660096e1
Dominick <d3lee@ucsd.edu>
2020-12-03 01:19:17 -0800

    ☕️ Worked on newsfeed

--- src/components/newsItem/index.js
+++ src/components/newsItem/index.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 import "./style.css";
+import testImg from "../../routes/common/images/HumaneSociety.jpg"
+import XIcon from "../../routes/common/images/icons/x"
 
 export class NewsItem extends Component {
     render() {
         return (
-            <div>
+            <div className="NewsItemWithDate">
                 <p>Today</p>
-                <p>Today</p>
+                <p>Nov 12</p>
+                <div className="NewsCard">
+                    <div className="newsCardTimes">
+                        <p>8:00 AM</p>
+                        <p>9:30 AM</p>
+                    </div>
+                    <img src={testImg} alt="OrgPhoto"></img>
+                    <div className="NewsCardText">
+                        <h1>Annual Event for Some Event</h1>
+                        <p>www.zoom.us/1234567890</p>
+                    </div>
+                    <div className="deleteNewsItem">
+                        <XIcon />
+                    </div>
+                </div>
                 
             </div>
         );
--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Search from '../../routes/common/images/searchIcon';
+import Search from '../../routes/common/images/icons/searchIcon';
 import "./searchBar.css";
 
 
