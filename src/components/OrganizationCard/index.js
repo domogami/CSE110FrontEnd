b360ebe14e5725fe35175a8ffbd44f2e435b4bfd
Dominick <d3lee@ucsd.edu>
2020-12-03 23:39:57 -0800

    Functional Modal for orgProfile

--- src/components/OrganizationCard/index.js
+++ src/components/OrganizationCard/index.js
@@ -4,6 +4,7 @@ import addIcon from "./+.svg";
 import followedIcon from "./Focus.svg";
 import "./orgCards.css"
 import OrganizationProfile from "../organizationProfile/index.js"
+import Modal from 'react-bootstrap-modal'
 
 /**
  * @extends {Component<{ doc: OrganizationDocument }>}
@@ -18,6 +19,12 @@ export class OrganizationCard extends Component {
             followed: false,
             logo: "https://cdn.vox-cdn.com/thumbor/zEZJzZFEXm23z-Iw9ESls2jYFYA=/89x0:1511x800/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/55717463/google_ai_photography_street_view_2.0.jpg",
         };
+    }
+
+    toggleOrgProfile() {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
     }
 
     updateFollow() {
@@ -41,8 +48,28 @@ export class OrganizationCard extends Component {
                         </h5>
                     </div>
                     <img onClick={e => this.updateFollow(e)} src={this.state.followed ? followedIcon : addIcon} alt="Add org" className="followButton" />
-                    {this.state.isOpen ? <div><OrganizationProfile/></div> : <div></div>}
+                    
                 </div>
+                
+                <Modal show={this.state.isOpen} className="OrgProfileModal">
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                        Modal heading
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h4>Centered Modal</h4>
+                        <p>
+                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                        consectetur ac, vestibulum at eros.
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button onClick={e => this.toggleOrgProfile(e)}>Close</button>
+                    </Modal.Footer>
+                </Modal>
+                
             </div>
         );
     }
