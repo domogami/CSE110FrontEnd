ee7af9ceb7e01d0ff63dffd731c998efec80f4f3
Dominick <d3lee@ucsd.edu>
2020-12-05 20:55:25 -0800

    🪲 Fixed header bugs

--- src/images/icons/index.js
+++ src/images/icons/index.js
@@ -4,5 +4,6 @@ import TagIcon    from "./tag.svg";
 import PenIcon    from "./pen.svg";
 import PinIcon    from "./pin.svg";
 import XIcon      from "./x.svg";
+import TrashIcon  from "./trash.svg"
 
-export { FilterIcon, SearchIcon, TagIcon, PenIcon, PinIcon, XIcon };
+export { FilterIcon, SearchIcon, TagIcon, PenIcon, PinIcon, XIcon, TrashIcon };
--- src/routes/findEvent/index.js
+++ src/routes/findEvent/index.js
@@ -15,6 +15,11 @@ class FindEvent extends Component {
                     <NavHeader parent={this}/>
                 </div>
                 <div className="EventView fade-in">
+                    {/* <div className="featuredEventCard">
+                        <img src={OrgProfile} alt="FeaturedImage"/>
+                        <h1>Featured Event Here!</h1>
+                        <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p1>
+                    </div> */}
                     <div className="eventGrid">
                         {
                             API.filtered.events.map(event => <EventCard doc={event} />)
