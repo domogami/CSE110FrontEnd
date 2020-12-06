ed8efe579d41cf1fa9807df5df322291a4cb1f3e
Daniel <43658735+DPKubeck@users.noreply.github.com>
2020-12-06 01:09:35 -0800

    Added organization stats basics, still needs work

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -20,18 +20,68 @@ class Home extends Component {
         });
     }
 
+
     render(){
+        var orgDoc;
+        if (API.isOrganization) orgDoc = API.me;
+
+        var total = 0;
+        var orgRating;
+        if (API.isOrganization) {
+            if(orgDoc.ratings.length == 0) {
+                orgRating = "No Ratings";
+            } else {
+                for (var i = 0; i < orgDoc.ratings.length; i++) {
+                    total += orgDoc.ratings[i].stars;
+                }
+                orgRating = total / orgDoc.ratings.length;
+            }
+        }
+
+        var orgEvents;
+        if (API.isOrganization) orgEvents = orgDoc.events.length;
+
+        var followers10to13 = 0;
+        var followers13to17 = 0;
+        var followers18to30 = 0;
+        var followers31to54 = 0;
+        var followers55plus = 0;
+        if (API.isOrganization) {
+            for(var k = 0; k < orgDoc.followers.length; k++) {
+                if (orgDoc.followers[k].age.equals("10-13")) followers10to13++;
+                else if (orgDoc.followers[k].age.equals("13-17")) followers13to17++;
+                else if (orgDoc.followers[k].age.equals("18-30")) followers18to30++;
+                else if (orgDoc.followers[k].age.equals("31-54")) followers31to54++;
+                else if (orgDoc.followers[k].age.equals("55+")) followers55plus++;
+            }
+        }
+
+        //TODO Clean this part up for the organization and idk make a graph for the followers, data should be there
+
         return (
             <div className="HomePage">
                 <div className="header">
                     <NavHeader />
                 </div>
+
                 <div className="homeView fade-in">
                     {API.isIndividual   && <IndividualProfile   doc={API.me} />}
                     {API.isOrganization && <OrganizationProfile doc={API.me} />}
+
                     <div className="newsFeed">
-                        <h1>News Feed</h1>
-                        {API.feed.map(event => <NewsItem event={event} />)}
+                        <div className="organizationPhoto">
+                            <img src={orgDoc.picture} />
+                        </div>
+                        <div className="organizationStats">
+                            {API.isOrganization && <h1>{orgDoc.title} Statistics</h1> }
+
+                            {API.isOrganization && <p>Followers: {orgDoc.followers.length}</p> }
+                            {API.isOrganization && <p>example: Followers 13 to 17: {followers13to17}</p> }
+                            {API.isOrganization && <p>Ratings: {orgRating}</p> }
+                            {API.isOrganization && <p>Total Events: {orgEvents}</p> }
+
+                            {API.feed.map(event => <NewsItem event={event} />)}
+                        </div>
                     </div>
                 </div>
             </div>
