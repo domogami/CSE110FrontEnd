c5ed10b2d1a651b156e426421d360d04e0a3b170
unduwapk <ukandage@ucsd.edu>
2020-12-06 03:13:26 -0800

    Danny&Unduwap late night styling; john help

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -43,47 +43,47 @@ export default function Filter({ parent }) {
                 onRequestClose={e => (e.stopPropagation(), setOpen(""))}
                 onAfterClose={() => API.filter(skills, causes, distance, 
                     orgFind ? "organization" : "events").then(() => parent.forceUpdate())}              
-                className="filterModal EventProfileModal"
+                className="outerFilterModal EventProfileModal"
             >
                 {
-                    (openedModal == "skills" || openedModal == "causes") ? <h2>Select Filters</h2> : <h2>Distance (within kilometers)</h2>
+                    (openedModal == "skills" || openedModal == "causes") ? 
+                        <h2 className="filterText">Select Filters</h2> : 
+                        <h2 className="filterText" >Distance ({distance} kilometers)</h2>
                 }
                 <div className="filterModal" >
-                    <div className="filterOptions">
-                        {
-                            openedModal == "skills" &&
-                            <Select 
-                                className="entryField" isMulti options={API.ValidSkills.map(v => ({ label: v, value: v }))} 
-                                theme={customTheme}
-                                onChange={value => setSkills(value.map(v => v.value))}
-                                defaultValue={skills.map(c => ({ label: c, value: c }))}
-                            />
-                        }
-                        {
-                            openedModal == "causes" &&
-                            <Select 
-                                className="entryField" isMulti options={API.ValidCauses.map(v => ({ label: v, value: v }))} 
-                                theme={customTheme}
-                                onChange={value => setCauses(value.map(v => v.value))}
-                                defaultValue={causes.map(c => ({ label: c, value: c }))}
-                            />
-                        }
-                        {
-                            openedModal == "distance" &&
-                            <div>
-                                <input type="range" min="10" max="10000" 
-                                    style={{ width: "90%" }}
-                                    defaultValue={distance}
-                                    onChange={e => setDistance(~~e.target.value)} />
-                                <span>{distance} km</span>
-                            </div>
-                        }
-                    </div>
-                    
-                    <div className="confirmButton" style={{ textAlign: "center" }}>
-                        <button className="button" onClick={() => setOpen("")}>Confirm</button>
-                    </div>
+
+                    {
+                        openedModal == "skills" &&
+                        <Select 
+                            className="entryField" isMulti options={API.ValidSkills.map(v => ({ label: v, value: v }))} 
+                            theme={customTheme}
+                            onChange={value => setSkills(value.map(v => v.value))}
+                            defaultValue={skills.map(c => ({ label: c, value: c }))}
+                        />
+                    }
+                    {
+                        openedModal == "causes" &&
+                        <Select 
+                            className="entryField" isMulti options={API.ValidCauses.map(v => ({ label: v, value: v }))} 
+                            theme={customTheme}
+                            onChange={value => setCauses(value.map(v => v.value))}
+                            defaultValue={causes.map(c => ({ label: c, value: c }))}
+                        />
+                    }
+                    {
+                        openedModal == "distance" &&
+                        <div className="distanceFilter">
+                            <input type="range" min="10" max="10000" 
+                                defaultValue={distance}
+                                onChange={e => setDistance(~~e.target.value)} />
+                        </div>
+                    }
                 </div>
+
+                <div className="confirmButton" style={{ textAlign: "center" }}>
+                    <button className="button" onClick={() => setOpen("")}>Confirm</button>
+                </div>
+
             </Modal>
         </div >
     )
